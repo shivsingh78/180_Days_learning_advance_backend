6fb2e42f2cffbc866ff160b80969bd1bacfe471f
shivsingh <shivsinghbaghel95@gmail.com>
2026-06-17 09:34:01 +0530

    fix: update server response message to include version

--- Day-3-github-action/src/index.js
+++ Day-3-github-action/src/index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8000;
 
 app.get("/", (req,res)=>{
-    return res.json({msg:"hello from server shiv singh baghel"})
+    return res.json({msg:"hello from server shiv singh baghel v2"})
 })
 app.listen(PORT,()=>{
     console.log("server is listening on http://localhost:8080"); 
